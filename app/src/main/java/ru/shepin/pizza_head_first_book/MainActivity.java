bcd9db141567749a76242b6965aa22b8afe9d719
Schepin Dmitriy <bright1308@gmail.com>
2021-12-27 16:56:05 +0300

    pizza: add provider to send

--- app/src/main/java/ru/shepin/pizza_head_first_book/MainActivity.java
+++ app/src/main/java/ru/shepin/pizza_head_first_book/MainActivity.java
@@ -1,19 +1,24 @@
 package ru.shepin.pizza_head_first_book;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
+
+import androidx.annotation.NonNull;
 
 public class MainActivity extends Activity {
+    private ShareActionProvider actionProvider;
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_share);
+        actionProvider = (ShareActionProvider) item.getActionProvider();
+        setIntent("This is exampleText");
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -28,9 +33,17 @@ public class MainActivity extends Activity {
             case R.id.action_settings:
 
                 return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void setIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        actionProvider.setShareIntent(intent);
+    }
+
 }
