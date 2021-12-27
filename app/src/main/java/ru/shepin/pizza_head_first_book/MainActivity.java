d620166acb00d47cbc524f07cc373adf81ef1195
Schepin Dmitriy <bright1308@gmail.com>
2021-12-27 12:05:40 +0300

    pizza: init project

---
+++ app/src/main/java/ru/shepin/pizza_head_first_book/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.shepin.pizza_head_first_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
