facf078e95e94b2e212be8a5c85d03fdcd2d66ce
Schepin Dmitriy <bright1308@gmail.com>
2021-12-27 15:59:20 +0300

    pizza: create OrderActivity.java and create intent for navigate

--- app/src/main/java/ru/shepin/pizza_head_first_book/MainActivity.java
+++ app/src/main/java/ru/shepin/pizza_head_first_book/MainActivity.java
@@ -1,10 +1,13 @@
 package ru.shepin.pizza_head_first_book;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +16,21 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+
+                return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
