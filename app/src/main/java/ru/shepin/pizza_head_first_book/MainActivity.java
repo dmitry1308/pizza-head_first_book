42d0c5fe03ee82bc0f8f4afa1c256ad65f0b4725
Schepin Dmitriy <bright1308@gmail.com>
2021-12-27 15:41:34 +0300

    pizza: add menu to MainActivity.java

--- app/src/main/java/ru/shepin/pizza_head_first_book/MainActivity.java
+++ app/src/main/java/ru/shepin/pizza_head_first_book/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
